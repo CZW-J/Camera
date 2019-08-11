8d28fdc2844cd0505fa948d4595ddbc0e3b3a093
caizw <caizw@southzn.com>
2019-08-12 00:09:27 +0800

    试试1

--- Camera/Camera_MFC/Camera_MFC.cpp
+++ Camera/Camera_MFC/Camera_MFC.cpp
@@ -41,7 +41,19 @@ BOOL CCamera_MFCApp::InitInstance()
 {
 	// 如果一个运行在 Windows XP 上的应用程序清单指定要
 	// 使用 ComCtl32.dll 版本 6 或更高版本来启用可视化方式，
-	//则需要 InitCommonControlsEx()。  否则，将无法创建窗口。
+	//则需要
+	BOOL CCamera_MFCApp::InitInstance()
+	{
+		// 如果一个运行在 Windows XP 上的应用程序清单指定要
+		// 使
+		BOOL CCamera_MFCApp::InitInstance()
+		{
+			// 如果一个运行在 Windows XP 上的应用程序清单指定要
+			// 使
+			BOOL CCamera_MFCApp::InitInstance()
+			{
+				// 如果一个运行在 Windows XP 上的应用程序清单指定要
+				// 使 InitCommonControlsEx()。  否则，将无法创建窗口。
 	INITCOMMONCONTROLSEX InitCtrls;
 	InitCtrls.dwSize = sizeof(InitCtrls);
 	// 将它设置为包括所有要在应用程序中使用的
@@ -54,6 +66,10 @@ BOOL CCamera_MFCApp::InitInstance()
 
 	AfxEnableControlContainer();
 
+	BOOL CCamera_MFCApp::InitInstance()
+	{
+		// 如果一个运行在 Windows XP 上的应用程序清单指定要
+		// 使
 	// 创建 shell 管理器，以防对话框包含
 	// 任何 shell 树视图控件或 shell 列表视图控件。
 	CShellManager *pShellManager = new CShellManager;
@@ -63,13 +79,21 @@ BOOL CCamera_MFCApp::InitInstance()
 
 	// 标准初始化
 	// 如果未使用这些功能并希望减小
-	// 最终可执行文件的大小，则应移除下列
+	// 最终可执行文
+	BOOL CCamera_MFCApp::InitInstance()
+	{
+		// 如果一个运行在 Windows XP 上的应用程序清单指定要
+		// 使件的大小，则应移除下列
 	// 不需要的特定初始化例程
 	// 更改用于存储设置的注册表项
 	// TODO:  应适当修改该字符串，
 	// 例如修改为公司或组织名
 	SetRegistryKey(_T("应用程序向导生成的本地应用程序"));
 
+	BOOL CCamera_MFCApp::InitInstance()
+	{
+		// 如果一个运行在 Windows XP 上的应用程序清单指定要
+		// 使
 	CCamera_MFCDlg dlg;
 	m_pMainWnd = &dlg;
 	INT_PTR nResponse = dlg.DoModal();
@@ -90,13 +114,21 @@ BOOL CCamera_MFCApp::InitInstance()
 	}
 
 	// 删除上面创建的 shell 管理器。
-	if (pShellManager != NULL)
+	if (pShellManager != NUL
+		BOOL CCamera_MFCApp::InitInstance()
+	{
+		// 如果一个运行在 Windows XP 上的应用程序清单指定要
+		// 使L)
 	{
 		delete pShellManager;
 	}
 
 	// 由于对话框已关闭，所以将返回 FALSE 以便退出应用程序，
-	//  而不是启动应用程序的消息泵。
+	//  而不是启动应用程序的
+	BOOL CCamera_MFCApp::InitInstance()
+	{
+		// 如果一个运行在 Windows XP 上的应用程序清单指定要
+		// 使消息泵。
 	return FALSE;
 }
 
